26fe29b80e80bd96706627496b64c68af5117084
Nisarat Aunreunngam <nisarata@Nisarats-MacBook-Pro.local>
2024-02-05 21:32:14 +0700

    sound

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import c from "./keysnotes/C1.mp3";</script>
+<script>
+const playSound = () => {
+  const audio = new Audio(c);
+  audio.play();
+};
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <div
+      id="c-1-note"
+      class="flex justify-center bg-blue-100 w-1/12 p-auto"
+      @click="playSound"
+    >
+      c
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
